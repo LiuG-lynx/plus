431a8fbc8a6d4b35127397ef8bc609a687902a1e
201406007 <201406007@qq.com>
2017-09-14 12:49:16 +0800

    test2

--- calculator.py
+++ calculator.py
@@ -1,32 +1,56 @@
 #!/usr/bin/env python3
+
 import sys
-try:
-    a = sys.argv[1]
-    b = int(a) - 3500
-    if int(a) < 0:
-        raise ValueError()
-    if len(sys.argv) != 2:
-        raise ValueError()
-    if b <= 0:
-        c = 0
-    elif b <= 1500:
-        c = (b * 0.03) - 0
-    elif b <= 4500 and b > 1500:
-        c = (b * 0.1) - 105
-    elif b <= 9000 and b > 4500:
-        c = (b * 0.2) - 555
-    elif b <= 35000 and b > 9000:
-        c = (b * 0.25) - 1005
-    elif b <= 55000 and b > 35000:
-        c = (b * 0.3) - 2755
-    elif b <= 80000 and b > 35000:
-        c = (b * 0.35) - 5505
-    elif b > 80000:
-        c = b * 0.45 - 13505
-    print(format(float(c), ".2f"))
-    
-except ValueError:
-    print("Parameter Error")
-except IndexError:
-    print("Parameter Error")
-    
+def shuru(a):
+    del a[0]
+    b = ':'.join(a).split(':')
+    m = b[::2]
+    n = b[1::2]
+    c = []
+    for i in n:
+        c.append(int(i));
+    n =c
+    return m,n 
+def jisuan(n,m):
+	x = []
+	for i in n:
+		if i < 0:
+			raise ValueError()
+		l = (float(i) * 0.165)
+		b = i - 3500
+		d = i - 3500 - l
+		if b <= 0:
+			c = 0
+		elif b <= 1500:
+			c = (d * 0.03)
+		elif b <= 4500 and b > 1500:
+			c = (d * 0.1) - 105
+		elif b <= 9000 and b > 4500:
+			c = (d * 0.2) - 555
+		elif b <= 35000 and b > 9000:
+			c = (d * 0.25) - 1005
+		elif b <= 55000 and b > 35000:
+			c = (d * 0.3) - 2755
+		elif b <= 80000 and b > 55000:
+			c = (d * 0.35) - 5505
+		elif b > 80000:
+			c = (d * 0.45) - 13505
+		x.append(i - l - c)
+        #print(x)
+	    #print(m)
+	    #print(format(float(x), ".2f"))
+	for j in range(len(m)):
+	    print('{}:{:.2f}'.format(m[j],x[j]))
+
+
+
+if __name__ == '__main__':
+	try:
+	    m,n = shuru(sys.argv)
+	    jisuan(n,m)
+	except ValueError:
+		print("Parameter Error")
+	except IndexError:
+		print("Parameter Error")
+	
+
